16cbfc54e992f90a47f119e6cde5462306f48332
ntmf <paulo2012@GMAIL.COM>
2016-07-14 16:48:17 -0300

    cap9

--- livraria/src/br/com/caelum/livraria/bean/AutorBean.java
+++ livraria/src/br/com/caelum/livraria/bean/AutorBean.java
@@ -1,11 +1,15 @@
 package br.com.caelum.livraria.bean;
 
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 
 import br.com.caelum.livraria.dao.DAO;
 import br.com.caelum.livraria.modelo.Autor;
 
 @ManagedBean
+@ViewScoped
 public class AutorBean {
 
 	private Autor autor = new Autor();
@@ -14,9 +18,22 @@ public class AutorBean {
 		return autor;
 	}
 
+	public void alterarAutor(Autor autor){
+		this.autor = autor;
+	}
+	
 	public void gravar() {
-		System.out.println("Gravando autor " + this.autor.getNome());
+		System.out.println("Gravando autor " + this.autor.getNome() +" - "+this.autor.getId()) ;
 
-		new DAO<Autor>(Autor.class).adiciona(this.autor);
+		if(this.autor.getId() == null) new DAO<Autor>(Autor.class).adiciona(this.autor);
+		else new DAO<Autor>(Autor.class).atualiza(this.autor);
+	}
+	
+	public List<Autor> getAutores(){
+		return new DAO<Autor>(Autor.class).listaTodos();
+	}
+	
+	public void removerAutor(Autor autor){
+		new DAO<Autor>(Autor.class).remove(autor);
 	}
 }
